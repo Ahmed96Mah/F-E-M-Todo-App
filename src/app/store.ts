0613608b2ad327d3c1581f2633272fd433fb3bc2
Ahmed Mahmoud Abdalwahab <ahmed96mahmoud1996@gmail.com>
2022-10-24 17:57:05 +0200

    Add redux store

---
+++ src/app/store.ts
@@ -0,0 +1,24 @@
+import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
+import { todosReducer } from '../reducers/todosReducer';
+import { filterReducer } from '../reducers/filterReducer';
+import { themeReducer } from '../reducers/themeReducer';
+
+export const store = configureStore({
+  reducer: {
+    todos: todosReducer,
+    filter: filterReducer,
+    theme: themeReducer,
+  },
+  //@ts-ignore
+  middleware: [thunk],
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
