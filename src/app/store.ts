1da4143be91b31a8f844ae5c5a7f7d53ddb0263a
Ahmed Mahmoud Abdalwahab <ahmed96mahmoud1996@gmail.com>
2022-10-24 21:05:28 +0200

    Update store reducers

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,12 +3,14 @@ import thunk from 'redux-thunk';
 import { todosReducer } from '../reducers/todosReducer';
 import { filterReducer } from '../reducers/filterReducer';
 import { themeReducer } from '../reducers/themeReducer';
+import { loadingReducer } from '../reducers/loadingReducer';
 
 export const store = configureStore({
   reducer: {
     todos: todosReducer,
     filter: filterReducer,
     theme: themeReducer,
+    loading: loadingReducer,
   },
   //@ts-ignore
   middleware: [thunk],
