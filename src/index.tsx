5e78919c2f261d12230bab16e39a35a01da6bc8a
Ahmed Mahmoud Abdalwahab <ahmed96mahmoud1996@gmail.com>
2022-10-24 18:07:33 +0200

    Add index component

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
+import { ConnectedApp } from './App';
+import './css/index.css';
+
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+
+root.render(
+  <Provider store={store}>
+    <ConnectedApp />
+  </Provider>
+);
